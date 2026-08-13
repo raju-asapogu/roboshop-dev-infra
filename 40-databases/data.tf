dafdc9ee5acc5e19f382544e5128d1028c405109
raju-asapogu <asapoguraju@gmail.com>
2026-08-13 10:49:23 +0530

    terraform

---
+++ 40-databases/data.tf
@@ -0,0 +1,29 @@
+
+data "aws_ami" "joindevops" {
+    owners = ["973714476881"]
+    most_recent = true
+
+    filter {
+        name = "name"
+        values = ["Redhat-9-DevOps-Practice"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    filter {
+        name = "root-device-type"
+        values = ["ebs"]
+    }
+}
+
+data "aws_ssm_parameter" "mongodb_sg_id" {
+  name = "/${var.project_name}/${var.environment}/mongodb_sg_id" 
+}
+
+
+data "aws_ssm_parameter" "database_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/database_subnet_ids" 
+}
