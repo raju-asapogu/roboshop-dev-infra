624d3ec5f9598d3fe4721bdd7afd0b2682e0a69b
raju-asapogu <asapoguraju@gmail.com>
2026-08-13 14:49:25 +0530

    terraform

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -31,9 +31,9 @@ data "aws_ssm_parameter" "rabbitmq_sg_id" {
   name = "/${var.project_name}/${var.environment}/rabbitmq_sg_id" 
 }
 
-data "aws_ssm_parameter" "mysql_sg_id" {
-  name = "/${var.project_name}/${var.environment}/mysql_sg_id" 
-}
+# data "aws_ssm_parameter" "mysql_sg_id" {
+#   name = "/${var.project_name}/${var.environment}/mysql_sg_id" 
+# }
 
 data "aws_ssm_parameter" "database_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/database_subnet_ids" 
