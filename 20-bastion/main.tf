335fbd505312daae0b901edca7b2c74a207e6fa6
raju-asapogu <asapoguraju@gmail.com>
2026-08-17 16:38:15 +0530

    terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "bastion" {
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.bastion_sg_id]
     subnet_id = local.public_subnet_id
-
+    iam_instance_profile = aws_iam_instance_profile.bastion.name
     user_data = file("bastion.sh")
     
     tags = merge(
@@ -16,4 +16,7 @@ resource "aws_instance" "bastion" {
 }
 
 
-
+resource "aws_iam_instance_profile" "bastion" {
+  name = "bastion"
+  role = "BastionTerraformAdmin"
+}
