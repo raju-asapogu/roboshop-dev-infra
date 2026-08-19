503462da8449009c56eab76549dd320dd94060f4
raju-asapogu <asapoguraju@gmail.com>
2026-08-19 08:41:13 +0530

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -2,6 +2,7 @@ locals {
   common_name_suffix = "${var.project_name}-${var.environment}"  #roboshop-dev
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
   ami_id = data.aws_ami.joindevops.id
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   common_tags = {
