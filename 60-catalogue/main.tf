fc0fd455a5fca2d7e023ac2bf77725454bcbe3c7
raju-asapogu <asapoguraju@gmail.com>
2026-08-24 10:52:07 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -95,6 +95,9 @@ resource "aws_launch_template" "catalogue" {
 
   vpc_security_group_ids = [local.catalogue_sg_id]
 
+  # when we run terraform apply again, a new version will be created with new AMI ID
+  update_default_version = true
+
   # tags attached to the instance
   tag_specifications {
     resource_type = "instance"
@@ -196,13 +199,13 @@ resource "aws_lb_listener_rule" "catalogue" {
   }
 }
 
-# resource "terraform_data" "catalogue_local" {
-#   triggers_replace = [
-#     aws_instance.catalogue.id
-#   ]
+resource "terraform_data" "catalogue_local" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
   
-#   depends_on = [aws_autoscaling_policy.catalogue]
-#   provisioner "local-exec" {
-#     command = "aws ec2 terminate-instances --instance-ids ${aws_instance.catalogue.id}"
-#   }
-# }
+  depends_on = [aws_autoscaling_policy.catalogue]
+  provisioner "local-exec" {
+    command = "aws ec2 terminate-instances --instance-ids ${aws_instance.catalogue.id}"
+  }
+}
