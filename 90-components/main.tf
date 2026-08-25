f25fc4ca420d83468b3e60c435358198afaeb141
raju-asapogu <asapoguraju@gmail.com>
2026-08-25 15:04:40 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,5 +1,5 @@
 module "components" {
-  source = "../terraform-roboshop-component"
+  source = "../../terraform-roboshop-component"
   component = var.component
   rule_priority = var.rule_priority
 }
